cca760f279c2dcf580c7add7cfb7fc9cb8be253b
Anushka Singh <anushka14engineer@gmail.com>
2024-09-23 20:35:22 +0530

    Second

---
+++ Binary_Search.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+int BinarySearch(int arr[],int n,int key){
+    //start,end,mid
+    int start=0,end=n-1,mid;
+    while(start<=end){
+        // mid ko find kro
+        mid=(start+end)/2;
+        //arr[mid]==key
+        if(arr[mid]==key){
+            return mid;
+        }
+        //arr[mid]<key
+        else if(arr[mid]<key)
+        start=mid+1;
+        //arr[mid]>key
+        else
+        end=mid-1;
+    }
+    return -1;
+}
+int main(){
+    int arr[1000];
+    int n;
+    cout<<"Enter the size of array";
+    cin>>n;
+    cout<<"Enter elements in array";
+    for(int i=0;i<n;i++)
+    cin>>arr[i];
+
+    int key;
+    cout<<"Enter the key";
+    cin>>key;
+
+    cout<<BinarySearch(arr,n,key);
+
+
+    return 0;
+}
+
+// for mid we can write
+//         mid=start+(end-start)/2;
